3a21013863826a35909f2f90e9d7f0e7668dd446
Rohit Sahu <rohit.blockchaindeveloper@gmail.com>
2018-06-04 00:30:11 +0530

    Update deployfrontend.sh

--- deployfrontend.sh
+++ deployfrontend.sh
@@ -1,4 +1,4 @@
-rsync -r /home/rohit/Vote-Through-Token/ docs/
+rsync -r /home/rohit/Vote-Through-Tokens/ docs/
 git add .
 git commit -m "Compiles assets for Github Pages"
 git push -u origin master
